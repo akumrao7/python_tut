88beb1f6a656f45393cc28edeb939e233fbb5a09
akumrao7 <anujkumar.umrao@gmail.com>
2026-02-20 17:13:37 +0200

    ASSIGNMENT 1 Done

---
+++ Assign_1.py
@@ -0,0 +1,28 @@
+# Task 1: Perform Basic Mathematical Operations
+
+num1=int(input("Enter first number: "))
+
+num2=int(input("Enter second number: "))
+
+Add=num1+num2
+
+Sub= num1 - num2
+
+Mult=num1*num2
+
+Div = num1/ num2
+
+print("Addition:", Add)
+print("Subtraction:", Sub)
+print("Multiplication:", Mult)
+print("Division:", Div)
+
+
+
+# Task 2: Create a Personalized Greeting
+
+name=input("Enter your first name: ")
+lastname=input("Enter your last name: ")
+
+print("Congratulations!", name, lastname, "for your first code.")
+
--- README.md
+++ README.md
@@ -1,1 +1,3 @@
-# python_tut_mod3
+# This repository contains Python practice task:
+
+# Task 1 and Task 2 of ASSIGNMENT 1 is in file name: Assign_1.py
