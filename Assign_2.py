a38ec720c328d6e5f9c1b7fc85b8355daab93dd1
akumrao7 <anujkumar.umrao@gmail.com>
2026-03-03 15:40:58 +0200

    ASSIGNMENT 2 Done

---
+++ Assign_2.py
@@ -0,0 +1,19 @@
+
+# Task 1: Check if a Number is Even or Odd
+
+num = int(input("Enter a number: "))
+if num % 2 == 0:
+    print(num, "is an even number.")
+else:
+    print(num, "is an odd number.")
+
+
+# Task 2: Sum of Integers from 1 to 50 Using a Loop
+
+sum = 0
+for i in range(1,51):
+    sum = sum + i
+
+print(f"The sum of the numbers from 1 to 50 is: {sum}")
+
+
--- README.md
+++ README.md
@@ -1,3 +1,12 @@
 # This repository contains Python practice task:
 
-# Task 1 and Task 2 of ASSIGNMENT 1 is in file name: Assign_1.py
+# Task 1 and Task 2 of ASSIGNMENT 2 is in file name: Assign_2.py
+
+## How to Run
+
+1. Make sure Python is installed.
+2. Open Command Prompt in the project folder.
+3. Run the program:
+
+```bash
+python Assign_1.py
